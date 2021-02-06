29eddc0a43990f14075b79f139828782d7c9ca70
choopong.c <cchoopong@gmail.com>
2021-02-06 10:33:54 +0700

    refactor customer model

--- app/model/customer.go
+++ app/model/customer.go
@@ -5,9 +5,9 @@ type Customers struct {
 }
 
 type Customer struct {
+	CustomerForCreate
 	ID   int    `json:"id" example:"1"` // Customer ID
 	Hash string `swaggerignore:"true"`
-	CustomerForCreate
 }
 
 type CustomerForCreate struct {
@@ -19,5 +19,5 @@ type CustomerForUpdate struct {
 	Firstname string `json:"firstname" binding:"required" example:"Choopong" maxLength:"255"`   // Customer Firstname
 	Lastname  string `json:"lastname" binding:"required" example:"Choosamer" maxLength:"255"`   // Customer Lastname
 	Email     string `json:"email" binding:"required" example:"choo@gmail.com" maxLength:"255"` // Customer E-mail
-	Gender    string `json:gender example:"male" enums:"male,female"`                           // Customer Gender
+	Gender    string `json:"gender" example:"male" enums:"male,female"`                         // Customer Gender
 }
